e047af0b390dd993a087172dcfd93a4a68aed158
nadiaflorenahounou-lab <nadiaflorenahounou@gmail.com>
2025-12-27 01:31:41 +0000

    Initial commit - fichiers du site

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Année dynamique dans le footer
+document.getElementById ("Flore").textContent = "Bienvenue sur SHE IS THE CODE!!!";
+
+document.getElementById("WELCOME").textContent =
+  new Date().getFullYear();
